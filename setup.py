397321b9340e47d508b8dea3e88ba8ecec5aafc0
Hassan Mehmood <hm.hassanmehmood@gmail.com>
2024-04-24 20:29:29 +0500

    update setup.py and add requirements.txt file

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import find_packages, setup
 
+#!/usr/bin/env python
+
+import pathlib
+
+import pkg_resources
+
+
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
 setup(
     name="quickstart_etl",
     packages=find_packages(exclude=["quickstart_etl_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "boto3",
-        "pandas",
-        "matplotlib",
-    ],
+    install_requires=install_requires,
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
